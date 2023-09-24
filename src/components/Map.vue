67dd95ca02b104c30049a198eb4ae6ea7100428e
Jesse Phipps <jesaiahphipps@gmail.com>
2023-09-24 13:45:27 -0500

    added route list

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,23 +1,38 @@
 <template>
+  <div class="mapContainer">
+    <div class="routeList">
+      <h3>{{ active && active.name || "Select Route" }}</h3>
+      <div :key="r.name" v-for="(r) in routes">
+        <button @click="changeRoute(r)" :class="{active: r.active }">{{ r.name }}</button>
+      </div>
+      <div class="jsonloader">
+        <div>
+          <textarea v-model="jsoninput" id="" cols="30" rows="10"></textarea>
+        </div>
+        <button @click="loadjson">Load</button>
+      </div>
+    </div>
     <div class="map">
-      <div style="height:400px; width:600px">
+      <div style="height:600px; width:auto">
         <l-map ref="map" v-model:zoom="zoom" :center="[37.5997592, -93.4091279]">
           <l-tile-layer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            layer-type="base"
-            name="OpenStreetMap"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          layer-type="base"
+          name="OpenStreetMap"
           ></l-tile-layer>
-          <l-marker :lat-lng="markerLatLng" ></l-marker>
           <l-geo-json :geojson="route"></l-geo-json>
         </l-map>
       </div>
     </div>
-  </template>
+  </div>
+</template>
   
   
   <script>
   import "leaflet/dist/leaflet.css";
-  import  DtoT from '@/data/DunneganparkToTaylor.json'
+  import DtoT from '@/data/DunneganparkToTaylor.json'
+  import TtoF from '@/data/TaylorToFreddys.json'
+  import CtoW from '@/data/CemetaryToWoods.json'
   import { LMap, LTileLayer, LMarker, LGeoJson } from "@vue-leaflet/vue-leaflet";
   export default {
     components: {
@@ -26,20 +41,67 @@
       LMarker,
       LGeoJson
     },
+    onMounted(){
+ 
+    },
     data() {
       return {
+        active: null,
         zoom: 12,
         markerLatLng: [37.5997592, -93.4091279],
-        route: DtoT
+        route: null,
+        routes: [
+          {data: DtoT, name: "Dunnegan to Taylor", active: false},
+          {data: TtoF, name: "Taylor to Freddy's", active: false},
+          {data: CtoW, name: "Cemetary to Woods", active: false},
+          
+        ],
+        jsoninput: null
+
       };
     },
+    methods: {
+      changeRoute(newRoute){
+        this.active = newRoute
+        this.route = newRoute.data
+      },
+      loadjson(){
+        try{
+        this.route = JSON.parse(this.jsoninput)
+        this.active = {
+          name: "Custom Route"
+        }}
+        catch(e){
+          alert("invalid json!")
+        }
+      }
+    }
   };
   </script>
   
 <style>
-    .map {
-      display: flex; 
+    .mapContainer {
+      display: flex;
       justify-content: center;
+    }
+    .map {
+      flex:2 1 auto;  
+    }
+    .jsonloader {
+      flex-direction: column;
+      padding: 10px;
+    }
+    .routeList {
+      color: blue;
+      flex-direction: column;
+      flex: 1 1 auto;
+    }
+    .routeList button{
+      width: 51%;
+    }
+    .active {
+      font-weight: bold;
+      text-decoration: underline;
     }
 </style>
   
