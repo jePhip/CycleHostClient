696538b71152753ce9f4508730b54f216d3f4999
Jesse Phipps <jesaiahphipps@gmail.com>
2023-10-09 15:57:51 -0500

    refined /add, added delete

--- src/views/AddRoute.vue
+++ src/views/AddRoute.vue
@@ -1,40 +1,53 @@
 <template>
-    <form @submit.prevent="handleSubmit">
-      <label>Route Name</label>
-      <br>
-      <input type="text" v-model="routeName" required>
-      <br>
-      <label>Upload File</label>
-      <br>
-      <input type="file" ref="file" @change="gpxToJSON(file)">
-      <br>
-      <button>Submit</button>
-    </form>
-
-    <div class="routeList">
-      <div :key="r.name" v-for="(r) in routes">
-        <p>{{ r.name }} ID: {{ r.id }}</p>
+  <div class="add-route-container"> 
+      <form class="routeForm" @submit.prevent="handleSubmit">
+        <div class="input">
+          <label>Route Name </label>
+          <input type="text" name="routeName" v-model="routeName" required>
+        </div>
+        <div class="input">
+          <label>Upload File </label>
+          <input type="file" ref="file" @change="gpxToJSON(file)" required>
+        </div>
+        <div class="input">        
+          <button class="button">Submit</button>
+        </div>
+      </form>
+      <div class="routeList">
+        <h2>Routes: </h2>
+        <div class="route" :key="r.name" v-for="(r) in routes">
+          <p>{{ r.name }} ID: {{ r.id }}</p>
+          <button @click="deleteRoute(r.id)">DELETE</button>
+        </div>
       </div>
     </div>
+
 
   </template>
   
   <script>
   export default {
     async mounted(){
-      console.log("mounted")
       await this.fetchRoutes()
-      console.log(this.routes)
     },
     data() {
       return {
           newRoute: null,
           routeName: '',
-          gpx: '',
+          gpx: '',//TODO: add gpx to string parsing
           routes: null,
       }
     },
     methods: {
+      async deleteRoute(id) {
+        // DELETE request using fetch with async/await
+        let response = await fetch(`http://localhost:3000/v1/geo/${id}`, { method: 'DELETE' });
+        response = await response.json();
+        this.routes = this.routes.filter(r => {//update route list
+          return r.id !==  Number(response.id)
+        })
+        
+      },
         async fetchRoutes() {//to display
         try {
           let response = await fetch('http://localhost:3000/v1/geo');//eventually change to env variable
@@ -61,18 +74,25 @@
                 let text = await blob.text()
                 const newJSON = new DOMParser().parseFromString(text, "text/xml")
                 const converted = tj.gpx(newJSON);
-                this.newRoute = converted //<--
+                this.newRoute = converted //<-- place converted json into data object
                 return converted
                 };
                 reader.readAsText(selectedFile);
             }
         },
-      handleSubmit() {
-        console.log(this.routeName, this.newRoute)
-        this.postRoute(this.newRoute, this.routeName, this.gpx)
+      async handleSubmit() {
+        try {
+          console.log(this.routeName, this.newRoute)
+          const response = await this.postRoute(this.newRoute, this.routeName, this.gpx);
+          if(response && response.message === 'success!') {//have backend return response object with code?
+            await this.fetchRoutes();
+          }
+        } catch (error) {
+          console.log('error', error)
+        }
       },
-      postRoute( ){
-        fetch(`http://localhost:3000/v1/geo/`, {
+      async postRoute( ){
+        let response = await fetch(`http://localhost:3000/v1/geo/`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
@@ -85,10 +105,61 @@
 
           })
         })
+        response = await response.json();
+        return response;
       },
     }
   }
   </script>
 
 <style>
+
+.add-route-container {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+
+.add-route-container .routeForm {
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  background: lavenderblush;
+  margin: 0 0 10px 0;
+}
+
+.add-route-container .routeForm .input label {
+  text-align: left;
+  margin-right: 10px;
+}
+
+.add-route-container .routeList {
+  text-align: center;
+  background: teal;
+  max-width: 500px;
+  padding: 10px;
+}
+
+.routeList h2 {
+  color: white;
+}
+
+.routeList .route {
+  border: 3px solid grey;
+  background: #eee;
+  margin: 10px 0;
+  padding: 10px;
+  
+}
+
+.input {
+  text-align: center;
+  padding: 20px;
+}
+
+
+button.button {
+  width: 100px;
+  
+}
 </style>
