1ae90a3df8a56dc533a983a2e1634d0aac0290d7
Gemma Moore <GemmaMoore2021@gmail.com>
2023-10-30 23:30:09 -0500

    centered login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,18 +1,21 @@
 
 <template> 
    <div>   
-        <h2>Admin Login</h2>       
+             
         <!-- login form for admin-->
         <div class="centered-container">
-            <v-form v-model="form" ref="form" class="login-form">             
-                <!-- text field to get username from admin -->
-                <v-text-field label="Username" v-model="title"  :rules="inputRules" type="input"></v-text-field>
-                <!-- text field to get password from admin-->
-                <v-text-field label="Password" type="input" v-model="content" hint="Enter your password to access routes"></v-text-field>
-                <!-- button to submit the form and complete login-->
-                <br>
-                <v-btn flat class="success mx-0 mt-3" @click="submit" type="submit">Sign In</v-btn>
-            </v-form> 
+            <h2>Admin Login</h2>    
+            <v-sheet>       
+                <v-form v-model="form" ref="form" class="login-form">             
+                    <!-- text field to get username from admin -->
+                    <v-text-field label="Username" v-model="title"  :rules="inputRules" type="input"></v-text-field>
+                    <!-- text field to get password from admin-->
+                    <v-text-field label="Password" type="input" v-model="content" hint="Enter your password to access routes"></v-text-field>
+                    <!-- button to submit the form and complete login-->
+                    <br>
+                    <v-btn flat class="success mx-0 mt-3" @click="submit" type="submit">Sign In</v-btn>
+                </v-form>
+            </v-sheet>           
         </div>    
     </div>   
 </template>
@@ -39,6 +42,13 @@ methods: {
 </script>
 
 <style> 
+
+.centered-container
+{ 
+    margin: auto;
+    width: 50%;
+    text-align: center;
+}
 
 
 
