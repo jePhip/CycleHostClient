edc5f0b27f4c62ad45d379d84782dd5e610db550
JessePhipps <64932609+JessePhipps@users.noreply.github.com>
2023-10-31 09:26:09 -0500

    quick commit

--- src/App.vue
+++ src/App.vue
@@ -13,14 +13,35 @@
 <script>
 import Footer from '@/components/Footer.vue'
 import NavBar from '@/components/NavBar.vue'
+import {useRouteStore} from '@/store/index.js'
 export default {
   name: 'App',
   components: {
     Footer,
     NavBar
   },
+  mounted(){
+   
+  },
+  setup(){
+    const routeStore = useRouteStore()
+    routeStore.getRoutes()
+    return{routeStore}
+  },
   data: () => ({
     //
+    routeToAddApp: 
+      {
+        route: ``,
+        terrain: `dirt`,
+        length: 12341234,
+        difficulty: `Beginner`,
+        desc: `this new route is cool`,
+        name: `new name`,
+        gpx: ``,
+      
+    }
   }),
+
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,67 +1,64 @@
 import { defineStore } from "pinia";
 
-export const useRoutesStore = defineStore("routes", {
+export const useRouteStore = defineStore("routeStore", {
   state: () => ({ routes: [] }),
   getters: {
     getRoute(id) {
       return this.routes.find((r) => r.id == id);
     },
-    actions: {
-      async addRoute(
-        newRoute,
-        routeName,
-        gpx,
-        routeLength,
-        terrain,
-        difficulty,
-        routeDesc
-      ) {
-        routeToAdd = {
-          route: newRoute,
-          name: routeName,
-          gpx: gpx,
-          length: routeLength,
-          terrain: terrain,
-          difficulty: difficulty,
-          desc: routeDesc,
-        };
-        this.routes.push(routeToAdd);
-        let response = await fetch(`http://localhost:3000/v1/geo/`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(routeToAdd),
-        });
-        response = await response.json();
-        return response;
-      },
-      async removeRoute(id) {
-        //this.routes.remove((r) => r.id == id)//to update routes array in state
-        let response = await fetch(`http://localhost:3000/v1/geo/${id}`, {
-          method: "DELETE",
-        });
-        response = await response.json();
-        this.routes = this.routes.filter((r) => {
-          //update route list
-          return r.id !== Number(response.id);
-        });
-      },
-      //methods to mutate data
-      async initFetchRoutes() {
-        try {
-          let response = await fetch("http://localhost:3000/v1/geo"); //eventually change to env variable
-          response = await response.json();
-          this.routes = response.routes.map((r) => {
-            return {
-              ...r,
-              route: JSON.parse(r.route),
-            };
-          });
-        } catch (error) {
-          console.error(error);
+  },
+  actions: {
+    //methods to mutate data
+    async updateRoute(updatedRoute) {
+      //TODO: make this function work.. untested
+      id = updatedRoute.id; //take in whatever attributes are being updated, leave rest unchanged, put back into state
+      let response = await fetch(`http://localhost:3000/v1/geo/${id}`, {
+        method: "PUT",
+      });
+      response = await response.json();
+      this.routes = this.routes.map((r) => {
+        //update route list
+        if ((r.id = updatedRoute.id)) {
+          r = updatedRoute;
         }
-      },
+      });
+    },
+    async addRoute(routeToAdd) {//tested
+      this.routes.push(routeToAdd)
+      let response = await fetch(`http://localhost:3000/v1/geo/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(routeToAdd),
+      });
+      response = await response.json();
+      return response;
+    },
+    async removeRoute(id) {//tested
+      //this.routes.remove((r) => r.id == id)//to update routes array in state
+      let response = await fetch(`http://localhost:3000/v1/geo/${id}`, {
+        method: "DELETE",
+      });
+      response = await response.json();
+      this.routes = this.routes.filter((r) => {
+        //update route list
+        return r.id !== Number(response.id);
+      });
+    },
+    async getRoutes() {//tested
+      try {
+        let response = await fetch("http://localhost:3000/v1/geo"); //eventually change to env variable
+        response = await response.json();
+        this.routes = response.routes.map((r) => {
+          return {
+            ...r,
+            route: JSON.parse(r.route),
+          };
+        });
+      } catch (error) {
+        console.error(error);
+      }
     },
   },
 });
