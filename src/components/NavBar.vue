5e5d7824b83d2ee1bf39d7c1dd79562aa22c42f9
Gemma Moore <GemmaMoore2021@gmail.com>
2023-11-03 23:35:37 -0500

    added responsive hamburger menu

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,9 @@
 <template>
-  <nav :elevation="24">
+ 
+ <nav :elevation="24">
     <v-container justify-center class="navbar">
+      
+      
       <v-toolbar  class="toolbar" flat app>
         <v-img class="logo" src="@/assets/logo.jpg" />
         <v-spacer>
@@ -14,32 +17,25 @@
           </v-list-tile>
         </v-list>
         </v-spacer>
-      </v-toolbar>
+      </v-toolbar>        
+    
 
-      <v-navigation-drawer
-        class="hamburger"
-        v-model="drawer"
-        location="bottom"
-        temporary       
-      >
-        <v-list>
-            <v-list-tile v-for="link in links" :key="link.text">
-              <v-list-tile-content>
-                <v-btn class="white--text" router :to="link.route">{{
+      <v-menu  class="hamburger">
+        <template  v-slot:activator="{ props }">
+          <v-btn class="hamburger" icon="mdi-menu" color="#083a8c" v-bind="props"></v-btn>
+        </template>
+        <v-list class="menu-list">        
+          <v-list-tile v-for="link in links" :key="link.text">            
+                <v-btn variant="plane" class="white--text" router :to="link.route">{{
                   link.text
-                }}</v-btn>
-              </v-list-tile-content>
-            </v-list-tile>
-          </v-list>       
-
-      </v-navigation-drawer>
-
+                }}</v-btn>            
+          </v-list-tile>        
+        </v-list>
+      </v-menu>
 
 
     </v-container>    
-  </nav>
-
- 
+  </nav> 
 </template>
 
 <script>
