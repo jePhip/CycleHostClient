3f8b62bd7497825a1714e048f9fbe816f14e019e
Gemma Moore <GemmaMoore2021@gmail.com>
2024-01-15 15:10:00 -0600

    added responsiveness for cards

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <Map />
   <h1>start cards</h1>
   <v-container fluid>
-      <v-col v-for="r in routes" :key="r" cols="4">
+      <v-col v-for="r in routes" :key="r" cols="12" sm="6" md="4" lg="3">
         <RouteCard :route="r" />
       </v-col>
   </v-container>
