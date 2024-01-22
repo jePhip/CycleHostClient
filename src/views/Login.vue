c6cc239159b1b92cea83182bdbf200862d5c845f
Jesse Phipps <jesaiahphipps@gmail.com>
2024-01-22 13:43:23 -0600

    changed request

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,21 +1,21 @@
 <template>
-    <div id="Login">
-      <h1>Login Form</h1>
-      <form @submit.prevent="login">
-        <label for="username">Username:</label>
-        <input v-model="username" type="text" id="username" required>
-  
-        <label for="password">Password:</label>
-        <input v-model="password" type="password" id="password" required>
-  
-        <button type="submit">Login</button>
-      </form>
-      <p v-if="error" style="color: red;">{{ error }}</p>
-      <p v-if="message" style="color: green;">{{ message }}</p>
-    </div>
-  </template>
+  <div id="Login">
+    <h1>Login Form</h1>
+    <form @submit.prevent="login">
+      <label for="username">Username:</label>
+      <input v-model="username" type="text" id="username" required />
 
-<script> 
+      <label for="password">Password:</label>
+      <input v-model="password" type="password" id="password" required />
+
+      <button type="submit">Login</button>
+    </form>
+    <p v-if="error" style="color: red">{{ error }}</p>
+    <p v-if="message" style="color: green">{{ message }}</p>
+  </div>
+</template>
+
+<script>
 export default{
     data(){
         return{
@@ -31,30 +31,27 @@ export default{
     async login() {
       this.error = "";
       this.message = "";
+        let valbody = {
+            username: this.username,
+            password: this.password
+        }
       try {
         let response = await fetch(
           `http://localhost:3000/v1/user/`,
           {
-            method: "GET",
+            method: "POST",
             headers: {
-              "Content-Type": "text/plain",
+              "Content-Type": "application/json",
             },
+            body: JSON.stringify(valbody),
           }
         );
-        response = response.json();
-        this.message = "success";
+        response = await response.json();
+        this.message = response.message
         console.log(response)
       } catch (error) {
         this.error = "Invalid username or password";
         console.error("Login failed", error);
-      }
-    },
-    validation() {
-      if (
-        this.preusername == this.username &&
-        this.prepassword == this.password
-      ) {
-        validate = true;
       }
     },
   },
