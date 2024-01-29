142954ab73149c566a3f0ff38158895f6c6615e1
Gemma Moore <GemmaMoore2021@gmail.com>
2024-01-28 22:34:34 -0600

    file posts to console

--- src/views/RouteSuggestion.vue
+++ src/views/RouteSuggestion.vue
@@ -1,42 +1,77 @@
-<template> 
-
-    <v-form @submit.prevent="submit">
+<template>
+    <v-container>
+      <v-container class="description">
+        Please use the form below if you have a route you would like to see
+        included in the Bolivar Cycling website!
+        <br />
+        Please provide a name for the route along with its associated GPX file.
+        <br />
+        Your submission will be reviewed by the City of Bolivar.
+        <br />
+      </v-container>
+  
+      <form
+        class="formContainer"
+        @submit.prevent="sendEmail"
+        enctype="multipart/form-data"
+        id="form"
+        name="form"
+        ref="form"
+      >
         <v-text-field
-            v-model="name"
-            label="Name"
-            required
-        />
-        
+          label="Route Name"
+          prepend-icon="mdi-bike"
+          v-model="name"
+          name="name"
+          id="name"
+          required
+        >
+        </v-text-field>
+  
         <v-file-input
-           
-            label="GPX File"
-            accept=".gpx"
-            required
-            type="file"
-            ref="file"
-            
+          label="Upload .gpx File"
+          variant="outlined"
+          type="file"
+          ref="file"
+          @change="handleFile()"
+          required
+          name="file"
+          accept=".gpx"
         ></v-file-input>
-
-        <v-btn type="submit">Submit</v-btn>  
-    
-    </v-form>
-
-</template>
-
-<script setup>
-
-import { ref } from 'vue';
-
-
-    let file = ref();
-    let handleFile = () => {
-        let fileContent = file.value.files[0];
-        console.log(fileContent);
-    }
-
-    
-</script>
-
-<style> 
-
-</style>
+  
+        <v-btn class="submit" type="submit">submit</v-btn>
+      </form>
+    </v-container>
+  </template>
+  
+  <script>
+  
+  export default {
+    name: "routeSuggestion",
+    data() {
+      return {
+        name: "",
+        file: null,
+      };
+    },
+  
+    methods: {
+      handleFile() {
+        const selectedFile = this.$refs.file.files[0];
+        console.log(selectedFile);
+      },
+      
+    },
+  };
+  </script>
+  
+  <style>
+  .formContainer {
+    max-width: 500px;
+    color: #083a8c;
+  }
+  
+  .description {
+    text-align: center;
+  }
+  </style>
