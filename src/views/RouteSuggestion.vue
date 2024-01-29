0e43817821e6a430e218d816f8ec78cd03c9bd9f
Gemma Moore <GemmaMoore2021@gmail.com>
2024-01-28 20:48:54 -0600

    adde route suggestion form

--- src/views/RouteSuggestion.vue
+++ src/views/RouteSuggestion.vue
@@ -1,9 +1,40 @@
 <template> 
 
+    <v-form @submit.prevent="submit">
+        <v-text-field
+            v-model="name"
+            label="Name"
+            required
+        />
+        
+        <v-file-input
+           
+            label="GPX File"
+            accept=".gpx"
+            required
+            type="file"
+            ref="file"
+            
+        ></v-file-input>
+
+        <v-btn type="submit">Submit</v-btn>  
+    
+    </v-form>
+
 </template>
 
-<script>
+<script setup>
 
+import { ref } from 'vue';
+
+
+    let file = ref();
+    let handleFile = () => {
+        let fileContent = file.value.files[0];
+        console.log(fileContent);
+    }
+
+    
 </script>
 
 <style> 
