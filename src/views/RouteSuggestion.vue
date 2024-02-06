de9c19fd7991e246a0f4233cceae15d357ffd31c
Jesse Phipps <jesaiahphipps@gmail.com>
2024-02-05 23:12:40 -0600

    changed hosting

--- src/views/RouteSuggestion.vue
+++ src/views/RouteSuggestion.vue
@@ -1,15 +1,17 @@
 <template>
-    <v-container>
-      <v-container class="description">
-        Please use the form below if you have a route you would like to see
-        included in the Bolivar Cycling website!
-        <br />
-        Please provide a name for the route along with its associated GPX file.
-        <br />
-        Your submission will be reviewed by the City of Bolivar.
-        <br />
-      </v-container>
-  
+  <v-container>
+    <v-container class="description">
+      Please use the form below if you have a route you would like to see
+      included in the Bolivar Cycling website!
+      <br />
+      Please provide a name for the route along with its associated GPX file.
+      <br />
+      Your submission will be reviewed by the City of Bolivar.
+      <br />
+    </v-container>
+  </v-container>
+  <v-row justify="center">
+    <v-col cols="3">
       <form
         class="formContainer"
         @submit.prevent="handleSubmit"
@@ -27,7 +29,7 @@
           required
         >
         </v-text-field>
-  
+
         <v-file-input
           label="Upload .gpx File"
           variant="outlined"
@@ -38,95 +40,79 @@
           name="file"
           accept=".gpx"
         ></v-file-input>
-  
+
         <v-btn @click="submit" class="submit" type="submit">submit</v-btn>
       </form>
-    </v-container>
-  </template>
-  
-  <script>
+    </v-col>
+  </v-row>
+</template>
 
-  export default {
-    name: "routeSuggestion",
-    data() {
-      return {
-        name: "",
-        file: null,
-      };
+<script>
+export default {
+  name: "routeSuggestion",
+  data() {
+    return {
+      name: "",
+      file: null,
+    };
+  },
+
+  methods: {
+    // handle file once uploaded
+    handleFile() {
+      this.file = this.$refs.file.files[0];
+      console.log(this.file);
     },
-  
-    methods: {
 
-      // handle file once uploaded 
-      handleFile() {
-        this.file = this.$refs.file.files[0];
-        console.log(this.file);
-        
-      },
-      
-      // send data to backend 
-      async postSuggestion()
-      { 
+    // send data to backend
+    async postSuggestion() {
+      const blob = new Blob([this.file], { type: "text/plain" });
+      const text = await blob.text();
+      console.log(text);
 
-        const blob = new Blob([this.file], { type: "text/plain" });
-        const text = await blob.text();
-        console.log(text);
-        
-
-       /* const formData = new FormData();
+      /* const formData = new FormData();
         formData.append('name', this.name);
         formData.append('route', text);
         console.log(formData);*/
 
-        
+      let response = await fetch("http://localhost:3000/v1/email/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: this.name,
+          file: text,
+        }),
+      });
+      //response = await response.json();
+      return response;
+    },
 
-        let response = await fetch('http://localhost:3000/v1/email/', {
-          method: "POST",
-          headers: 
-          { 
-            "Content-Type": "application/json",
-             
-          },
-          body: JSON.stringify({
-            name: this.name,
-            file: text
-          })
-          
-        })
-        //response = await response.json();
-        return response;
-      },
-
-      // subbmit 
-      async submit(){ 
-        try{
-          const response = await this.postSuggestion(
-            this.file,
-            this.name
-          );       
-          console.log(response);   
-          setTimeout(() => {
-            location.reload();
+    // subbmit
+    async submit() {
+      try {
+        const response = await this.postSuggestion(this.file, this.name);
+        console.log(response);
+        setTimeout(() => {
+          location.reload();
         }, 1000);
-        }
-        catch(e){
-          console.log("error:\n");
-          console.log(e);
-
-        }     
-
+      } catch (e) {
+        console.log("error:\n");
+        console.log(e);
       }
     },
-  };
-  </script>
-  
-  <style>
-  .formContainer {
-    max-width: 500px;
-    color: #083a8c;
-  }
-  
-  .description {
-    text-align: center;
-  }
-  </style>
+  },
+};
+</script>
+
+<style>
+.formContainer {
+  max-width: 500px;
+  color: #083a8c;
+}
+
+.description {
+  text-align: center;
+}
+</style>
